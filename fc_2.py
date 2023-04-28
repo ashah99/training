db1832c1e5c702a77c098d946ab2cb1c6672f582
ashah99 <83623002+ashah99@users.noreply.github.com>
2023-04-28 15:55:47 -0400

    Python FC Code 2 and 3 added

---
+++ fc_2.py
@@ -0,0 +1,12 @@
+def print_value(value):
+    """This function prints the value passed in"""
+    print ("Your value is:", value)
+
+
+print_value("Job Completed")
+
+print_value(350)
+
+name = "Teacher"
+print_value(name)
+
---
+++ fc_3.py
@@ -0,0 +1,13 @@
+def change_value(value):
+    """This function changes the value passed in to 1"""
+    print ("Inside, value is:", value)
+    value = 1
+    print ("Inside, value is changed to:", value)
+
+# Calling
+
+number = 5
+print ("Outside, number is:", number)
+change_value(number)
+print ("Outside, number is now:", number)
+
